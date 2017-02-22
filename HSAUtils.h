370768ab279768efc51520da98ea71148a90b27a
Chris Hesik <christopher.hesik@amd.com>
2017-02-21 22:12:21 -0500

    add doxygen comments

--- HSAUtils.h
+++ HSAUtils.h
@@ -13,11 +13,12 @@
 #include "HSAModule.h"
 #include "TSingleton.h"
 
-typedef std::vector<uint32_t> HSADeviceIdList;
+typedef std::vector<uint32_t> HSADeviceIdList; ///< typedef for a list of hsa devices
 
-class HSAUtils : public TSingleton < HSAUtils >
+/// Singleton HSA Utilities class
+class HSAUtils : public TSingleton<HSAUtils>
 {
-    friend class TSingleton < HSAUtils > ;
+    friend class TSingleton<HSAUtils>;
 
 public:
     /// constructor
@@ -26,13 +27,23 @@ public:
     // destructor
     ~HSAUtils();
 
+    /// Gets a list of device IDs for all GPU devices on the system
+    /// \param hsaDeviceIds the list of device IDs on the systems
+    /// \return true if the list of device ids was populated sucessfully; false otherwise
     bool GetHSADeviceIds(HSADeviceIdList& hsaDeviceIds);
 
 private:
+    /// Inits the HSA Module
+    /// \returns true on success
     bool InitHSAModule();
+
+    /// Callback function for hsa_iterate_agents
+    /// \param agent the hsa agent
+    /// \param pData the user-defined data
+    /// \return the hsa status
     static hsa_status_t GetGPUDevicesCallback(hsa_agent_t agent, void* pData);
 
-    HSAModule* m_pHsaModule;
+    HSAModule* m_pHsaModule; ///< the hsa module
 };
 
 #endif // _HSA_UTILS_H
